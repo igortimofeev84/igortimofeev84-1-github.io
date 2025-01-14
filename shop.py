4f2793d7a96bfb8e3a4c170631c8b95c7857334e
igortimofeev84 <igor_timofeev84@mail.ru>
2025-01-14 10:57:06 +0300

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,4 +1,4 @@
-[]######################## Отображение страницы товара #########################
+[]######################## Отображение страницы товара ##########################
 # from selenium.webdriver.common.by import By
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
